af46bfa1a6d43bde6ead9b92b7c078a28c496cac
KZTahat <kztahat96@gmail.com>
2021-07-01 15:38:05 +0300

    removing some profile things

--- src/components/Footers/SimpleFooter.js
+++ src/components/Footers/SimpleFooter.js
@@ -37,32 +37,19 @@ class SimpleFooter extends React.Component {
           <Container>
             <Row className=" row-grid align-items-center mb-5">
               <Col lg="6">
-                <h3 className=" text-primary font-weight-light mb-2">
+                {/* <h3 className=" text-primary font-weight-light mb-2">
                   Thank you for supporting us!
-                </h3>
+                </h3> */}
                 <h4 className=" mb-0 font-weight-light">
                   Let's get in touch on any of these platforms.
                 </h4>
               </Col>
               <Col className="text-lg-center btn-wrapper" lg="6">
-                <Button
-                  className="btn-icon-only rounded-circle"
-                  color="twitter"
-                  href="https://twitter.com/creativetim"
-                  id="tooltip475038074"
-                  target="_blank"
-                >
-                  <span className="btn-inner--icon">
-                    <i className="fa fa-twitter" />
-                  </span>
-                </Button>
-                <UncontrolledTooltip delay={0} target="tooltip475038074">
-                  Follow us
-                </UncontrolledTooltip>
+                {/* FaceBook Button */}
                 <Button
                   className="btn-icon-only rounded-circle ml-1"
                   color="facebook"
-                  href="https://www.facebook.com/creativetim"
+                  href="https://www.facebook.com/khaled.tahat.31/"
                   id="tooltip837440414"
                   target="_blank"
                 >
@@ -73,6 +60,8 @@ class SimpleFooter extends React.Component {
                 <UncontrolledTooltip delay={0} target="tooltip837440414">
                   Like us
                 </UncontrolledTooltip>
+
+                {/*  */}
                 <Button
                   className="btn-icon-only rounded-circle ml-1"
                   color="dribbble"
@@ -87,10 +76,11 @@ class SimpleFooter extends React.Component {
                 <UncontrolledTooltip delay={0} target="tooltip829810202">
                   Follow us
                 </UncontrolledTooltip>
+                {/* GitHub Button */}
                 <Button
                   className="btn-icon-only rounded-circle ml-1"
                   color="github"
-                  href="https://github.com/creativetimofficial"
+                  href="https://github.com/KZTahat/Portfolio-Khaled"
                   id="tooltip495507257"
                   target="_blank"
                 >
